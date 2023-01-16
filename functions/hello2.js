387f9b8d499e08073379259d0c5313923812ea1b
Venkatesh G <46915517+venky0195@users.noreply.github.com>
2023-01-16 12:12:58 +0530

    add more default exports

---
+++ functions/hello2.js
@@ -0,0 +1,15 @@
+import { helper } from "./helper";
+
+export default handler = (req, res) => {
+    res
+      .status(200)
+      .json({
+        query: req.query,
+        method: req.method,
+        body: req.body,
+        headers: req.headers,
+        url: req.url,
+        randomNumber: Math.floor((Math.random()*100) + 1),
+        helperText: helper()
+      })
+  }
