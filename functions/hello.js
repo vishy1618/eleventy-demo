eacc18620c14406fdd1733719ce2c574ba5beb71
Suvish Thoovamalayil <suvish.thoovamalayil@contentstack.com>
2023-02-16 16:21:43 +0530

    Add only one set-cookie header without an array

--- functions/hello.js
+++ functions/hello.js
@@ -4,9 +4,7 @@ export default function handler(req, res) {
     .status(200)
     .setHeader(
       'set-cookie',
-      [
-        '__Secure-next-auth.callback-url=https%3A%2F%2Fknowledge-base-development.devcontentstackapps.com; Path=/; HttpOnly; Secure; SameSite=Lax'
-      ]
+      '__Secure-next-auth.callback-url=https%3A%2F%2Fknowledge-base-development.devcontentstackapps.com; Path=/; HttpOnly; Secure; SameSite=Lax'
     )
     .json({
       "csrfToken": "1119eaf21d4f8ddf609c9a6243b0b0fb8a9213595d7ea8832470d4c570835c8a"
