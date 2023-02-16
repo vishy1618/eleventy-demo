e734199e6b1f4aa15e87b255fe85e86a4c681c39
Suvish Thoovamalayil <suvish.thoovamalayil@contentstack.com>
2023-02-16 16:11:59 +0530

    Add set cookie header for testing

--- functions/hello.js
+++ functions/hello.js
@@ -1,16 +1,15 @@
-import { helper } from "./helper";
-
 export default function handler(req, res) {
   console.log("Console log from function hello!!");
-    res
-      .status(200)
-      .json({
-        query: req.query,
-        method: req.method,
-        body: req.body,
-        headers: req.headers,
-        url: req.url,
-        randomNumber: Math.floor((Math.random()*100) + 1),
-        helperText: helper()
-      })
-  }
+  res
+    .status(200)
+    .setHeader(
+      'set-cookie',
+      [
+        '__Host-next-auth.csrf-token=1119eaf21d4f8ddf609c9a6243b0b0fb8a9213595d7ea8832470d4c570835c8a%7Cfdd42845f3639bfcd5ef01b5f55e1d23a6ea116cb380734cc28a60e647a2e046; Path=/; HttpOnly; Secure; SameSite=Lax',
+        '__Secure-next-auth.callback-url=https%3A%2F%2Fknowledge-base-development.devcontentstackapps.com; Path=/; HttpOnly; Secure; SameSite=Lax'
+      ]
+    )
+    .json({
+      "csrfToken": "1119eaf21d4f8ddf609c9a6243b0b0fb8a9213595d7ea8832470d4c570835c8a"
+    })
+}
