457bacc5e1dd40d790c1f91907ff4dd2108e64e4
Suvish Thoovamalayil <vishy1618@gmail.com>
2023-04-20 18:29:52 +0530

    Use node fetch

--- functions/hello.js
+++ functions/hello.js
@@ -1,12 +1,16 @@
+import fetch from 'node-fetch';
+
 export default function handler(req, res) {
-    res
-      .status(200)
-      .json({
-        query: req.query,
-        method: req.method,
-        body: req.body,
-        headers: req.headers,
-        url: req.url,
-        randomNumber: Math.floor((Math.random()*100) + 1)
-      })
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  console.log(await response.json());
+  res
+    .status(200)
+    .json({
+      query: req.query,
+      method: req.method,
+      body: req.body,
+      headers: req.headers,
+      url: req.url,
+      randomNumber: Math.floor((Math.random()*100) + 1)
+    });
   }
