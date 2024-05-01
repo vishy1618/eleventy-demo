6cb038ded165fc37f4b838b2ac76c63e578bd4b5
Suvish Thoovamalayil <suvish.thoovamalayil@contentstack.com>
2024-05-01 17:12:57 +0530

    Fix launch.json escapes

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const fs = require('fs');
 const json = `{
   "redirects": [
     {
-      "source": "/:path((?!platform-overview|pricing|_next|forms2|wp-json|.*\\.|.*\\?).+[^\\/]$)",
+      "source": "/:path((?!platform-overview|pricing|_next|forms2|wp-json|.*\\\\.|.*\\\\?).+[^\\\\/]$)",
       "destination": "/:path/",
       "statusCode": 301
     }
   ],
   "rewrites": [
     {
-      "source": "/robots\\.txt",
+      "source": "/robots\\\\.txt",
       "destination": "https://salesloftmarketing.github.io/RobotsFix/robots.txt"
     },
     {
-      "source": "/:path((?!platform-overview|pricing|_next|forms2|\\?[^s]).+)",
+      "source": "/:path((?!platform-overview|pricing|_next|forms2|\\\\?[^s]).+)",
       "destination": "https://live-salesloft-v2.pantheonsite.io/:path",
       "response": {
         "headers": {
