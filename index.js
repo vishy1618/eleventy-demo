951d6a991af2884c8a2ef0f56af7d54182b3a51f
Suvish Thoovamalayil <suvish.thoovamalayil@contentstack.com>
2024-05-01 17:08:18 +0530

    CL-998 | Check if build can generate launch.json

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const json = `{
+  "redirects": [
+    {
+      "source": "/:path((?!platform-overview|pricing|_next|forms2|wp-json|.*\\.|.*\\?).+[^\\/]$)",
+      "destination": "/:path/",
+      "statusCode": 301
+    }
+  ],
+  "rewrites": [
+    {
+      "source": "/robots\\.txt",
+      "destination": "https://salesloftmarketing.github.io/RobotsFix/robots.txt"
+    },
+    {
+      "source": "/:path((?!platform-overview|pricing|_next|forms2|\\?[^s]).+)",
+      "destination": "https://live-salesloft-v2.pantheonsite.io/:path",
+      "response": {
+        "headers": {
+          "x-robots-tag": "index, follow"
+        }
+      }
+    }
+  ]
+}`;
+
+fs.writeFileSync('launch.json', json);
+
